9da6fa25c20022bbb45dc716c9aad0ff80aa8346
Erwin Oldenkamp <oldenkamperwin@gmail.com>
2014-03-13 23:00:25 +0100

    Error in javascript. Must check later

--- src/main/java/org/jenkinsci/plugins/StashBranchParameter/StashBranchParameterDefinition.java
+++ src/main/java/org/jenkinsci/plugins/StashBranchParameter/StashBranchParameterDefinition.java
@@ -1,21 +1,54 @@
 package org.jenkinsci.plugins.StashBranchParameter;
 
+import hudson.Extension;
 import hudson.model.ParameterDefinition;
 import hudson.model.ParameterValue;
+import hudson.util.FormValidation;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.*;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Property;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
+
+import javax.servlet.ServletException;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Created by erwin on 13/03/14.
  */
 public class StashBranchParameterDefinition extends ParameterDefinition {
 
-    public StashBranchParameterDefinition(String name) {
-        super(name);
-    }
+    private String username;
 
-    public StashBranchParameterDefinition(String name, String description) {
+    private String password;
+
+    private String stashApiUrl;
+
+
+    @DataBoundConstructor
+    public StashBranchParameterDefinition(String name, String description, String stashApiUrl, String username, String password) {
         super(name, description);
+        this.username = username;
+        this.password = password;
+        this.stashApiUrl = stashApiUrl;
     }
 
     @Override
@@ -27,4 +60,94 @@ public class StashBranchParameterDefinition extends ParameterDefinition {
     public ParameterValue createValue(StaplerRequest staplerRequest) {
         return null;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStashApiUrl() {
+        return stashApiUrl;
+    }
+
+    public void setStashApiUrl(String stashApiUrl) {
+        this.stashApiUrl = stashApiUrl;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends ParameterDescriptor {
+        @Override
+        public String getDisplayName() {
+            return "Stash Branch Parameter";
+        }
+
+        public FormValidation doCheckUsername(@QueryParameter final String stashApiUrl, @QueryParameter final String username, @QueryParameter final String password) throws IOException, ServletException {
+            if(StringUtils.isBlank(stashApiUrl)) {
+                return FormValidation.ok();
+            }
+
+//            URL url = new URL(stashApiUrl);
+//
+//
+//            HttpHost target = new HttpHost(url.getHost(), url.getPort(), url.getProtocol());
+//            CredentialsProvider credsProvider = new BasicCredentialsProvider();
+//            credsProvider.setCredentials(
+//                    new AuthScope(target.getHostName(), target.getPort()),
+//                    new UsernamePasswordCredentials(username, password));
+//            CloseableHttpClient httpclient = HttpClients.custom()
+//                    .setDefaultCredentialsProvider(credsProvider).build();
+//
+//            try {
+//
+//                // Create AuthCache instance
+//                AuthCache authCache = new BasicAuthCache();
+//                // Generate BASIC scheme object and add it to the local
+//                // auth cache
+//                BasicScheme basicAuth = new BasicScheme();
+//                authCache.put(target, basicAuth);
+//
+//                // Add AuthCache to the execution context
+//                HttpClientContext localContext = HttpClientContext.create();
+//                localContext.setAuthCache(authCache);
+//
+//                HttpGet httpget = new HttpGet(url.getPath());
+//
+//                System.out.println("Executing request " + httpget.getRequestLine() + " to target " + target);
+//                for (int i = 0; i < 3; i++) {
+//                    CloseableHttpResponse response = httpclient.execute(target, httpget, localContext);
+//                    try {
+//                        return FormValidation.error("status code: {}",response.getStatusLine().getStatusCode());
+//
+//                    } finally {
+//                        response.close();
+//                    }
+//                }
+//            } finally {
+//                httpclient.close();
+//            }
+            return FormValidation.error("geen eind");
+        }
+
+        public FormValidation doCheckPassword(@QueryParameter final String stashApiUrl, @QueryParameter final String username, @QueryParameter final String password) throws IOException, ServletException {
+            return doCheckUsername(stashApiUrl, username, password);
+        }
+
+        public FormValidation doCheckStashApiUrl(@QueryParameter final String stashApiUrl, @QueryParameter final String username, @QueryParameter final String password) throws IOException, ServletException {
+            return doCheckUsername(stashApiUrl, username, password);
+        }
+
+
+    }
+
 }
