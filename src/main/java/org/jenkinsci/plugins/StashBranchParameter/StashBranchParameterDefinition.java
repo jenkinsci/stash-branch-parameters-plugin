c2fc4f62574786b6378319b5bae3a88c61fd8cb2
Erwin Oldenkamp <oldenkamperwin@gmail.com>
2014-03-23 19:49:01 +0100

    Finish first version of the plugin

--- src/main/java/org/jenkinsci/plugins/StashBranchParameter/StashBranchParameterDefinition.java
+++ src/main/java/org/jenkinsci/plugins/StashBranchParameter/StashBranchParameterDefinition.java
@@ -3,9 +3,14 @@ package org.jenkinsci.plugins.StashBranchParameter;
 import hudson.Extension;
 import hudson.model.ParameterDefinition;
 import hudson.model.ParameterValue;
+import hudson.model.StringParameterValue;
 import hudson.util.FormValidation;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import net.sf.json.util.JSONUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -16,6 +21,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.*;
 import org.apache.http.protocol.HttpContext;
@@ -29,13 +35,19 @@ import org.kohsuke.stapler.StaplerRequest;
 import javax.servlet.ServletException;
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.*;
+import java.util.logging.Logger;
 
 /**
  * Created by erwin on 13/03/14.
  */
 public class StashBranchParameterDefinition extends ParameterDefinition {
+
+    private static final Logger LOGGER = Logger.getLogger(StashBranchParameterDefinition.class.getName());
 
     private String username;
 
@@ -54,12 +66,15 @@ public class StashBranchParameterDefinition extends ParameterDefinition {
 
     @Override
     public ParameterValue createValue(StaplerRequest staplerRequest, JSONObject jsonObject) {
-        return null;
+        LOGGER.warning("Value "+ jsonObject.toString());
+        String value = jsonObject.getString("value");
+        return new StringParameterValue(this.getName(),value);
     }
 
     @Override
     public ParameterValue createValue(StaplerRequest staplerRequest) {
-        return null;
+        String[] parameterValues = staplerRequest.getParameterValues(getName());
+        return new StringParameterValue(this.getName(),parameterValues[0]);
     }
 
     public String getUsername() {
@@ -86,6 +101,87 @@ public class StashBranchParameterDefinition extends ParameterDefinition {
         this.stashApiUrl = stashApiUrl;
     }
 
+    public List<String> getDefaultValueMap() throws IOException {
+        return computeDefaultValueMap();
+    }
+
+    private List<String> computeDefaultValueMap() throws IOException {
+        List<String> list = new ArrayList<String>();
+        URL url = new URL(getStashApiUrl());
+
+        HttpHost target = new HttpHost(url.getHost(), url.getPort(), url.getProtocol());
+        CredentialsProvider credsProvider = new BasicCredentialsProvider();
+        credsProvider.setCredentials(
+                new AuthScope(target.getHostName(), target.getPort()),
+                new UsernamePasswordCredentials(getUsername(), getPassword()));
+        CloseableHttpClient httpclient = HttpClients.custom()
+                .setDefaultCredentialsProvider(credsProvider).build();
+
+        try {
+
+            AuthCache authCache = new BasicAuthCache();
+            BasicScheme basicAuth = new BasicScheme();
+            authCache.put(target, basicAuth);
+            HttpClientContext localContext = HttpClientContext.create();
+            localContext.setAuthCache(authCache);
+            HttpGet httpget = new HttpGet(url.getPath());
+
+            CloseableHttpResponse response = httpclient.execute(target, httpget, localContext);
+            try{
+                HttpEntity entity = response.getEntity();
+                StringWriter writer = new StringWriter();
+                IOUtils.copy(entity.getContent(), writer);
+
+                JSONObject json = JSONObject.fromObject(writer.toString());
+                if(json.has("values")){
+                    JSONArray values = json.getJSONArray("values");
+                    Iterator<JSONObject> iterator = values.iterator();
+                    while(iterator.hasNext()){
+                        JSONObject branch = iterator.next();
+                        if(branch.has("displayId")){
+                            list.add(branch.getString("displayId"));
+                        }
+                    }
+                }
+            }finally {
+                response.close();
+            }
+
+        } catch (IOException e) {
+            throw e;
+        }finally {
+            httpclient.close();
+        }
+        return list;
+    }
+
+    private CloseableHttpResponse getResponse(Map<String, Boolean> map) throws IOException {
+        URL url = new URL(getStashApiUrl());
+
+        HttpHost target = new HttpHost(url.getHost(), url.getPort(), url.getProtocol());
+        CredentialsProvider credsProvider = new BasicCredentialsProvider();
+        credsProvider.setCredentials(
+                new AuthScope(target.getHostName(), target.getPort()),
+                new UsernamePasswordCredentials(getUsername(), getPassword()));
+        CloseableHttpClient httpclient = HttpClients.custom()
+                .setDefaultCredentialsProvider(credsProvider).build();
+
+        try {
+            AuthCache authCache = new BasicAuthCache();
+            BasicScheme basicAuth = new BasicScheme();
+            authCache.put(target, basicAuth);
+            HttpClientContext localContext = HttpClientContext.create();
+            localContext.setAuthCache(authCache);
+            HttpGet httpget = new HttpGet(url.getPath());
+
+            CloseableHttpResponse response = httpclient.execute(target, httpget, localContext);
+                return response;
+
+        }finally {
+            httpclient.close();
+        }
+    }
+
     @Extension
     public static class DescriptorImpl extends ParameterDescriptor {
         @Override
@@ -97,9 +193,7 @@ public class StashBranchParameterDefinition extends ParameterDefinition {
             if(StringUtils.isBlank(stashApiUrl)) {
                 return FormValidation.ok();
             }
-
             URL url = new URL(stashApiUrl);
-
 
             HttpHost target = new HttpHost(url.getHost(), url.getPort(), url.getProtocol());
             CredentialsProvider credsProvider = new BasicCredentialsProvider();
@@ -110,25 +204,19 @@ public class StashBranchParameterDefinition extends ParameterDefinition {
                     .setDefaultCredentialsProvider(credsProvider).build();
 
             try {
-
-                // Create AuthCache instance
                 AuthCache authCache = new BasicAuthCache();
-                // Generate BASIC scheme object and add it to the local
-                // auth cache
                 BasicScheme basicAuth = new BasicScheme();
                 authCache.put(target, basicAuth);
-
-                // Add AuthCache to the execution context
                 HttpClientContext localContext = HttpClientContext.create();
                 localContext.setAuthCache(authCache);
-
                 HttpGet httpget = new HttpGet(url.getPath());
-
-                System.out.println("Executing request " + httpget.getRequestLine() + " to target " + target);
 
                 CloseableHttpResponse response = httpclient.execute(target, httpget, localContext);
                 try {
-                    return FormValidation.warning("status code: {}",response.getStatusLine().getReasonPhrase());
+                    if(response.getStatusLine().getStatusCode()!=200){
+                       return FormValidation.error("Authorization failed");
+                    }
+                    return FormValidation.ok();
 
                 } finally {
                     response.close();
@@ -136,10 +224,11 @@ public class StashBranchParameterDefinition extends ParameterDefinition {
             }
             catch(UnknownHostException e){
                 return FormValidation.error("Couldn't connect with server");
-            } finally {
+            }catch(HttpHostConnectException e){
+                return FormValidation.error("Couldn't connect with server");
+            }finally {
                 httpclient.close();
             }
-            //return FormValidation.ok();
         }
 
         public FormValidation doCheckPassword(@QueryParameter final String stashApiUrl, @QueryParameter final String username, @QueryParameter final String password) throws IOException, ServletException {
