b20a1e0909f2d0d521a8c16628e8a1cb77da0952
Erwin Oldenkamp <oldenkamperwin@gmail.com>
2014-03-24 22:05:14 +0100

    Add group utils for creating groups in the view

---
+++ src/main/java/org/jenkinsci/plugins/StashBranchParameter/MapsUtils.java
@@ -0,0 +1,34 @@
+package org.jenkinsci.plugins.StashBranchParameter;
+
+import java.util.*;
+
+/**
+ * Created by erwin on 24/03/14.
+ */
+public class MapsUtils {
+    public static Map<String,Map<String,String>> groupMap(Map<String, String> map) {
+        Set<Map.Entry<String, String>> entries = map.entrySet();
+
+        Map<String,Map<String,String>> result = new TreeMap<String, Map<String, String>>();
+
+        for(Map.Entry<String,String> entry: entries){
+            if(entry.getValue().contains("/")){
+                String group = entry.getValue().substring(0,entry.getValue().indexOf("/"));
+                String name = entry.getValue().substring(entry.getValue().indexOf("/")+1);
+                addToGroup(result,group,entry.getKey(),name);
+            }else{
+                addToGroup(result,"branch",entry.getKey(),entry.getValue());
+            }
+        }
+        return result;
+    }
+
+    private static void addToGroup(Map<String, Map<String, String>> result, String group, String key, String name) {
+        if(!result.containsKey(group)){
+            result.put(group,new TreeMap<String, String>());
+        }
+        result.get(group).put(key,name);
+    }
+
+
+}
