8ace8e08f05fab04928af5eab3021ef7b7b3bbca
Erwin Oldenkamp <oldenkamperwin@gmail.com>
2015-12-01 22:49:27 +0100

    reformat the code

--- src/main/java/org/jenkinsci/plugins/StashBranchParameter/StashBranchParameterDescriptor.java
+++ src/main/java/org/jenkinsci/plugins/StashBranchParameter/StashBranchParameterDescriptor.java
@@ -31,126 +31,151 @@ import java.net.UnknownHostException;
 import java.util.List;
 import java.util.Map;
 
-public class StashBranchParameterDescriptor extends ParameterDefinition.ParameterDescriptor {
-        private String username;
+public class StashBranchParameterDescriptor extends ParameterDefinition.ParameterDescriptor
+{
+	private String username;
 
-        private Secret password;
+	private Secret password;
 
-        private String stashApiUrl;
-        private String repo;
+	private String stashApiUrl;
+	private String repo;
 
-        public StashBranchParameterDescriptor() {
-            super(StashBranchParameterDefinition.class);
-            load();
-        }
+	public StashBranchParameterDescriptor()
+	{
+		super(StashBranchParameterDefinition.class);
+		load();
+	}
 
-        @Override
-        public String getDisplayName() {
-            return "Stash Branch Parameter";
-        }
+	@Override
+	public String getDisplayName()
+	{
+		return "Stash Branch Parameter";
+	}
 
-        public String getUsername() {
-            return username;
-        }
+	public String getUsername()
+	{
+		return username;
+	}
 
-        public void setUsername(String username) {
-            this.username = username;
-            save();
-        }
+	public void setUsername(String username)
+	{
+		this.username = username;
+		save();
+	}
 
-        public String getPassword() {
-            return password==null?null:password.getPlainText();
-        }
+	public String getPassword()
+	{
+		return password == null ? null : password.getPlainText();
+	}
 
-        public void setPassword(Secret password) {
-            this.password = password;
-        }
+	public void setPassword(Secret password)
+	{
+		this.password = password;
+	}
 
-        public String getStashApiUrl() {
-            return stashApiUrl;
-        }
+	public String getStashApiUrl()
+	{
+		return stashApiUrl;
+	}
 
-        public void setStashApiUrl(String stashApiUrl) {
-            this.stashApiUrl = stashApiUrl;
-        }
+	public void setStashApiUrl(String stashApiUrl)
+	{
+		this.stashApiUrl = stashApiUrl;
+	}
 
-        public String getRepo() {
-            return repo;
-        }
+	public String getRepo()
+	{
+		return repo;
+	}
 
-        public void setRepo(String repo) {
-            this.repo = repo;
-        }
+	public void setRepo(String repo)
+	{
+		this.repo = repo;
+	}
 
-        @Override
-        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-            stashApiUrl =      formData.getString("stashApiUrl");
-            username = formData.getString("username");
-            password = Secret.fromString(formData.getString("password"));
+	@Override
+	public boolean configure(StaplerRequest req, JSONObject formData) throws FormException
+	{
+		stashApiUrl = formData.getString("stashApiUrl");
+		username = formData.getString("username");
+		password = Secret.fromString(formData.getString("password"));
 
-            save();
-            return super.configure(req,formData);
-        }
+		save();
+		return super.configure(req, formData);
+	}
 
-        public FormValidation doCheckUsername(@QueryParameter final String stashApiUrl, @QueryParameter final String username, @QueryParameter final String password) throws IOException, ServletException {
-            if(StringUtils.isBlank(stashApiUrl)) {
-                return FormValidation.ok();
-            }
-            URL url = new URL(stashApiUrl);
+	public FormValidation doCheckUsername(@QueryParameter final String stashApiUrl, @QueryParameter final String username, @QueryParameter final String password) throws IOException, ServletException
+	{
+		if (StringUtils.isBlank(stashApiUrl))
+		{
+			return FormValidation.ok();
+		}
+		URL url = new URL(stashApiUrl);
 
-            HttpHost target = new HttpHost(url.getHost(), url.getPort(), url.getProtocol());
-            CredentialsProvider credsProvider = new BasicCredentialsProvider();
-            credsProvider.setCredentials(
-                    new AuthScope(target.getHostName(), target.getPort()),
-                    new UsernamePasswordCredentials(username, password));
-            CloseableHttpClient httpclient = HttpClients.custom()
-                    .setDefaultCredentialsProvider(credsProvider).build();
+		HttpHost target = new HttpHost(url.getHost(), url.getPort(), url.getProtocol());
+		CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		credsProvider.setCredentials(new AuthScope(target.getHostName(), target.getPort()), new UsernamePasswordCredentials(username, password));
+		CloseableHttpClient httpclient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
 
-            try {
-                AuthCache authCache = new BasicAuthCache();
-                BasicScheme basicAuth = new BasicScheme();
-                authCache.put(target, basicAuth);
-                HttpClientContext localContext = HttpClientContext.create();
-                localContext.setAuthCache(authCache);
-                HttpGet httpget = new HttpGet(url.getPath().concat("/repos"));
+		try
+		{
+			AuthCache authCache = new BasicAuthCache();
+			BasicScheme basicAuth = new BasicScheme();
+			authCache.put(target, basicAuth);
+			HttpClientContext localContext = HttpClientContext.create();
+			localContext.setAuthCache(authCache);
+			HttpGet httpget = new HttpGet(url.getPath().concat("/repos"));
 
-                CloseableHttpResponse response = httpclient.execute(target, httpget, localContext);
-                try {
-                    if(response.getStatusLine().getStatusCode()!=200){
-                        return FormValidation.error("Authorization failed");
-                    }
-                    return FormValidation.ok();
+			CloseableHttpResponse response = httpclient.execute(target, httpget, localContext);
+			try
+			{
+				if (response.getStatusLine().getStatusCode() != 200)
+				{
+					return FormValidation.error("Authorization failed");
+				}
+				return FormValidation.ok();
 
-                } finally {
-                    response.close();
-                }
-            }
-            catch(UnknownHostException e){
-                return FormValidation.error("Couldn't connect with server");
-            }catch(HttpHostConnectException e){
-                return FormValidation.error("Couldn't connect with server");
-            }finally {
-                httpclient.close();
-            }
-        }
+			}
+			finally
+			{
+				response.close();
+			}
+		}
+		catch (UnknownHostException e)
+		{
+			return FormValidation.error("Couldn't connect with server");
+		}
+		catch (HttpHostConnectException e)
+		{
+			return FormValidation.error("Couldn't connect with server");
+		}
+		finally
+		{
+			httpclient.close();
+		}
+	}
 
-        public FormValidation doCheckPassword(@QueryParameter final String stashApiUrl, @QueryParameter final String username, @QueryParameter final String password) throws IOException, ServletException {
-            return doCheckUsername(stashApiUrl, username, password);
-        }
+	public FormValidation doCheckPassword(@QueryParameter final String stashApiUrl, @QueryParameter final String username, @QueryParameter final String password) throws IOException, ServletException
+	{
+		return doCheckUsername(stashApiUrl, username, password);
+	}
 
-        public ListBoxModel doFillRepositoryItems() throws MalformedURLException {
-            StashConnector connector = new StashConnector(getStashApiUrl(),getUsername(),getPassword());
-            ListBoxModel items = new ListBoxModel();
-            Map<String, List<String>> repositories = connector.getRepositories();
+	public ListBoxModel doFillRepositoryItems() throws MalformedURLException
+	{
+		StashConnector connector = new StashConnector(getStashApiUrl(), getUsername(), getPassword());
+		ListBoxModel items = new ListBoxModel();
+		Map<String, List<String>> repositories = connector.getRepositories();
 
-            for(Map.Entry<String,List<String>> entry: repositories.entrySet()){
-                String project = entry.getKey();
-                for(String repo: entry.getValue()){
-                    String name = project.concat(" / ").concat(repo);
-                    String value = name.replace(" ","");
-                    items.add(new ListBoxModel.Option(name, value));
-                }
-            }
-            return items;
-        }
-    }
+		for (Map.Entry<String, List<String>> entry : repositories.entrySet())
+		{
+			String project = entry.getKey();
+			for (String repo : entry.getValue())
+			{
+				String name = project.concat(" / ").concat(repo);
+				String value = name.replace(" ", "");
+				items.add(new ListBoxModel.Option(name, value));
+			}
+		}
+		return items;
+	}
+}
